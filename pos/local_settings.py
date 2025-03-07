3b6a9ac5715b6193a4c15e60712f40dad90fe838
Chaser-254 <esibitaremmanuel316@gmail.com>
2025-03-07 22:51:29 +0300

    redeployment

--- pos/local_settings.py
+++ pos/local_settings.py
@@ -1,7 +1,9 @@
 import os
-import dj_database_url
+# import dj_database_url
 
-SECRET_KEY = os.getenv("SECRET_KEY", "your-default-secret-key")
+SECRET_KEY = 'django-insecure-504x&av9c7f3w2+ib5u9g2)w7+lwsba#)2%70c*nx(t!y&!e=w'
+
+# SECRET_KEY = os.getenv("SECRET_KEY", "your-default-secret-key")
 
 DEBUG = os.getenv('DEBUG', 'False') == 'True'
 
@@ -10,7 +12,17 @@ DEBUG = os.getenv('DEBUG', 'False') == 'True'
 ALLOWED_HOSTS = ['eve-pos-926d4500456b.herokuapp.com', '127.0.0.1', 'localhost']
 
 
-
 DATABASES = {
-    'default': dj_database_url.config(default=os.getenv("DATABASE_URL"))
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'pos',
+        'USER': 'postgres',
+        'PASSWORD': 'Manu@254#',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
+    }
 }
+
+# DATABASES = {
+#     'default': dj_database_url.config(default=os.getenv("DATABASE_URL"))
+# }
--- posApp/urls.py
+++ posApp/urls.py
@@ -5,9 +5,8 @@ from django.urls import path
 from django.views.generic.base import RedirectView
 
 urlpatterns = [
-    path('', views.home, name="home"), # Add this line
     path('redirect-admin', RedirectView.as_view(url="/admin"),name="redirect-admin"),
-    path('home/', views.home, name="home"),
+    path('', views.home, name="home-page"),
     path('login', auth_views.LoginView.as_view(template_name = 'posApp/login.html',redirect_authenticated_user=True), name="login"),
     path('userlogin', views.login_user, name="login-user"),
     path('logout', views.logoutuser, name="logout"),
